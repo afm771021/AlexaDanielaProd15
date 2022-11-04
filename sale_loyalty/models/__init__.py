97def4cf8a5de8b7a316256f7b7529f239af16dc
Emmanuel Guillen Olmos <ego@mss.mx>
2022-11-03 20:47:58 -0600

    v1.0 Joyeria AlexaDaniela

---
+++ sale_loyalty/models/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# Powered by Mastermind Software Services
+# © 2022 Mastermind Software Services (<https://www.mss.mx>).
+
+from . import sale_loyalty
+from . import res_config
+from . import res_company
+from . import res_partner
+from . import sale
+from . import product
